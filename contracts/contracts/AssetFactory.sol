714e8d3cbbed6911c0535415ec874b62e58cdf9f
Aryan <aryanamittiwari@gmail.com>
2026-02-06 23:02:09 +0530

    Initial commit

---
+++ contracts/contracts/AssetFactory.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./RealWorldAsset.sol";
+import "./MockUSDC.sol"; // Import MockUSDC
+
+contract AssetFactory {
+    RealWorldAsset[] public assets;
+    address public paymentTokenAddress; // The address of our Fake Money
+
+    event AssetCreated(address indexed assetAddress, string name, address indexed owner);
+
+    // We set the MockUSDC address when we deploy the Factory
+    constructor(address _paymentTokenAddress) {
+        paymentTokenAddress = _paymentTokenAddress;
+    }
+
+    function createAsset(
+        string memory _name,
+        string memory _symbol,
+        string memory _url,
+        uint256 _valuation
+    ) external {
+        RealWorldAsset newAsset = new RealWorldAsset(
+            _name,
+            _symbol,
+            _url,
+            _valuation,
+            msg.sender,
+            paymentTokenAddress // Pass the currency address
+        );
+
+        assets.push(newAsset);
+        emit AssetCreated(address(newAsset), _name, msg.sender);
+    }
+
+    function getDeployedAssets() external view returns (RealWorldAsset[] memory) {
+        return assets;
+    }
+}
---
+++ contracts/contracts/MockUSDC.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MockUSDC is ERC20, Ownable {
+    constructor(address initialOwner) ERC20("Mock USD Coin", "mUSDC") Ownable(initialOwner) {}
+
+    // Function to give users fake money (The "Stripe" webhook will call this)
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+    
+    // Allow decimals to match real USDC (usually 6, but we'll stick to 18 for simplicity)
+}
